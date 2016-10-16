8897ef130ff3147823fa06daf6a5448cb4ea8010
Sam Esgate <sam.esgate11@imperial.ac.uk>
2016-10-16 21:43:20 +0100

    It works with all continents! TODO: Use Redux. Add interactivity. Countries vs. Continents.

--- src/components.js
+++ src/components.js
@@ -3,31 +3,6 @@
 import React from 'react';
 import { List, Map } from 'immutable';
 
-export function Todo(props) {
-  const { todo } = props;
-  if(todo.isDone) {
-    return <strike>{todo.text}</strike>;
-  } else {
-    return <span>{todo.text}</span>;
-  }
-}
-
-export function TodoList(props) {
-  const { todos } = props;
-  return (
-    <div className='todo'>
-      <input type='text' placeholder='Add todo' />
-      <ul className='todo__list'>
-        {todos.map(t => (
-          <li key={t.id} className='todo__item'>
-            <Todo todo={t} />
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-
 function randomColor() {
   return '#' + ('00000'+(Math.random()*(1<<24)|0).toString(16)).slice(-6);
 }
@@ -47,24 +22,24 @@ function createCountryFromObject(country, width, height) {
   return createCountry(country.get('id'), country.get('name'), width, height);
 }
 
-function drawCountries(continents, maxWidth, maxHeight) {
+function drawCountries(countries, areaLeft, maxWidth, maxHeight) {
   // If there is one country, draw it as big as you can.
-  const size = continents.size;
+  const size = countries.size;
   if (size === 1) {
-    return createCountryFromObject(continents.getIn(0), maxWidth, maxHeight);
+    return createCountryFromObject(countries.getIn(0), maxWidth, maxHeight);
   }
 
   // If there are two countries, split into two based on their sizes.
   else if (size === 2) {
-    const cont1 = continents.get(0);
-    const cont2 = continents.get(1);
+    const cont1 = countries.get(0);
+    const cont2 = countries.get(1);
     const ratio = cont1.get('area') / (cont1.get('area') + cont2.get('area'));
     return (
       <div className="world_wrapper" style={{width: maxWidth + 'vw', height: maxHeight + 'vh'}}>
         {
           List([
             createCountryFromObject(cont1, maxWidth * ratio, maxHeight),
-            createCountryFromObject(cont2, maxWidth - (maxWidth * ratio), maxHeight)
+            createCountryFromObject(cont2, maxWidth * (1 - ratio), maxHeight)
           ])
         }
       </div>
@@ -73,40 +48,66 @@ function drawCountries(continents, maxWidth, maxHeight) {
 
   // If there are three, draw one, recurse other two.
   else if (size == 3) {
-    const cont1 = continents.get(0);
-    const cont2 = continents.get(1);
-    const cont3 = continents.get(2);
+    const cont1 = countries.get(0);
+    const cont2 = countries.get(1);
+    const cont3 = countries.get(2);
     const ratio = cont1.get('area') / (cont1.get('area') + cont2.get('area') + cont3.get('area'));
-    const width1 = Math.floor(maxWidth * ratio);
     return (
-      <div className="world_wrapper">
+      <div className="world_wrapper" style={{width: maxWidth + 'vw', height: maxHeight + 'vh'}}>
         {
           List([
             createCountryFromObject(cont1, maxWidth * ratio, maxHeight),
-            drawCountries(continents.shift(), maxWidth - (maxWidth * ratio), maxHeight)
+            drawCountries(countries.shift(), areaLeft * (1 - ratio), maxWidth * (1 - ratio), maxHeight)
           ])
         }
       </div>
     )
   }
 
+  else if (size >= 4) {
+    const cont1 = countries.get(0);
+    const cont2 = countries.get(1);
+    const ratio = ((cont1.get('area') + cont2.get('area')) / areaLeft);
+    /* 1  2
+       3  rest
+       */
+    return (
+      <div className="world_wrapper_vertical" style={{width: maxWidth + 'vw', height: maxHeight + 'vh'}}>
+        {
+          List([
+            drawCountries(countries.setSize(2), areaLeft * ratio, maxWidth, maxHeight * ratio),
+            drawCountries(countries.shift().shift(), areaLeft * (1 - ratio), maxWidth, maxHeight * (1 - ratio))
+          ])
+        }
+      </div>
+    )
+
+  }
+
   return (
     <div className="world_wrapper">
-      {continents.map(function(continent) {
-        return createCountry(continent.get('id'), continent.get('name'), 20, 20);
-      })}
+      {
+        countries.map(function(continent) {
+          return createCountry(continent.get('id'), continent.get('name'), 20, 20);
+        })
+      }
     </div>
   )
 }
 
 export function WorldMap(props) {
-  const { continents, parentWidth, parentHeight } = props;
+  const { countries, parentWidth, parentHeight } = props;
   let maxWidth = (parentWidth === undefined) ? 100 : parentWidth;
   let maxHeight = (parentHeight === undefined) ? 100 : parentHeight;
 
-  if (continents.isEmpty()) {
+  if (countries.isEmpty()) {
     return null;
   }
 
-  return drawCountries(continents, maxWidth, maxHeight);
+  let totalArea = 0;
+  countries.forEach(function(country) {
+    totalArea += country.get('area');
+  });
+
+  return drawCountries(countries, totalArea, maxWidth, maxHeight);
 }
