5f8cec7f4a28f5adda21a05f9661dd74e08cfccf
Sam Esgate <sam.esgate11@imperial.ac.uk>
2016-10-17 15:25:00 +0100

    Extended countries to have continent data. Added filter by continent. Currently static. Need to add new react component to control it.

--- src/app.js
+++ src/app.js
@@ -2,26 +2,25 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { List, Map } from 'immutable';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { Map } from 'immutable';
 
+import data from './data';
 import WorldMap from './world-map';
 
-const init = List([
-  Map({id: 0, name: 'Asia',                 area: 44579000}),
-  Map({id: 1, name: 'Africa',               area: 30065000}),
-  Map({id: 2, name: 'North America',        area: 24256000}),
-  Map({id: 3, name: 'South America',        area: 17819000}),
-  Map({id: 4, name: 'Antarctica',           area: 13209000}),
-  Map({id: 5, name: 'Europe',               area:  9938000}),
-  Map({id: 6, name: 'Australia + Oceania',  area:  2968000})
-]);
+const init = Map({
+  filter: 'Ocean',
+  countries: data
+});
 
 function reducer(state = init, action) {
   switch (action.type) {
     case 'REMOVE_COUNTRY':
-      return state.filter((country) => country.get('id') !== action.id);
+      return state.set('countries', state.get('countries').filter((country) => country.get('id') !== action.id));
+    case 'FILTER_CONTINENT':
+      return 
+        state.set('filter', action.filter);
     default:
       return state;
   }
